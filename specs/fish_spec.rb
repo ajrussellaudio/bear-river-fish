afccad6ba9e409ad81b383f215ab85b5eb4d59df
Alan Russell <ajrussellaudio@gmail.com>
2018-01-31 09:29:43 +0000

    add fish name test

---
+++ specs/fish_spec.rb
@@ -0,0 +1,16 @@
+require("minitest/autorun")
+require("minitest/rg")
+
+require_relative("../fish")
+
+class FishTest < MiniTest::Test
+
+  def setup
+    @fish1 = Fish.new("Wanda")
+  end
+
+  def test_fish_name
+    assert_equal("Wanda", @fish1.name)
+  end
+
+end
